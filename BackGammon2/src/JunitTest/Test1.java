934c33c94c4e3dbc1ff2fe7c85c7cf6b21fec830
galye <galye@192.168.56.89>
2024-12-30 17:52:51 +0200

    create and fix 6 junit tests

--- BackGammon2/src/JunitTest/Test1.java
+++ BackGammon2/src/JunitTest/Test1.java
@@ -11,46 +11,9 @@ import javafx.embed.swing.JFXPanel;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class TestFirst {
-
-    //Basic check that SysData ok - get SysData class
-	private static SysData check;
-
-    @BeforeClass
-    public static void setUpClass() throws InterruptedException {
-        // Initialize JavaFX Toolkit
-        new JFXPanel(); // Ensures JavaFX platform initializes
-        // Run on JavaFX Thread
-        Platform.runLater(() -> check = SysData.getInstance());
-        Stage stage = new Stage();
-        // Wait for JavaFX Thread to complete
-        Thread.sleep(1000);
-    }
-
-    @Test
-    public void testSysData() {
-        assertNotNull("SysData instance should not be null", check);
-    }
-
-
-    /*SysData check = SysData.getInstance();
-    @Test
-    public void testSysData() {
-    	assertNotNull(check);
-    }*/
-    
-    //need to fill this test
-	@Test
-	public void validRollDice() {
-		fail("Not yet implemented");
-	}
+public class Test1 {
 	
-    //need to fill this test
-	public void validPlayerNames() {
-		fail("Not yet implemented");
-	}
-	
-	    private GamePlay gamePlay;
+    	private GamePlay gamePlay;
 	    private GridPane[] gridPanes;
 	    private Stage stage;
 
@@ -63,32 +26,6 @@ public class TestFirst {
 	            gridPanes[i] = new GridPane();
 	        }
 	        gamePlay = new GamePlay(gridPanes, stage, "Medium");
-	    }
-
-	    //Ensures GamePlay initializes correctly with the specified difficulty.
-	    @Test
-	    public void testInitialization() {
-	        assertNotNull("GamePlay object should not be null", gamePlay);
-	        assertEquals("Difficulty should be set to Medium", "Medium", GamePlay.difficulty);
-	    }
-
-	    //Verifies random question spots are selected within a valid range.
-	    @Test
-	    public void testQuestionSpotSelection() {
-	        int[] questions = GamePlay.getQuestions();
-	        assertEquals("There should be 3 question spots selected", 3, questions.length);
-
-	        for (int question : questions) {
-	            assertTrue("Question spot should be in valid range (0-23)", question >= 0 && question < 24);
-	        }
-	    }
-
-	    //Tests placing a pawn upwards.
-	    @Test
-	    public void testSurpriseSpotSelection() {
-	        if (GamePlay.difficulty.equals("Hard")) {
-	            assertTrue("Surprise spot should be in valid range (0-23)", GamePlay.surprise >= 0 && GamePlay.surprise < 24);
-	        }
 	    }
 
 	    //Tests placing a pawn upwards.
